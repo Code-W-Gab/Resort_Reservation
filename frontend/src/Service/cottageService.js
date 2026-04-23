b88cc928031ed1857e7f0f25135e2a947176d9fc
Code-W-Gab <gabconcepcion02@gmail.com>
2026-04-23 23:05:32 +0800

    Make Service for update and getById cottage

--- frontend/src/Service/cottageService.js
+++ frontend/src/Service/cottageService.js
@@ -2,4 +2,6 @@ import api from '../Api/axios'
 
 export const AddCottage = (CottageName, Type, Descriptions, Capacity, DayTourPrice, OvernightPrice, Amenities) => api.post('/cottage/add', {CottageName, Type, Descriptions, Capacity, DayTourPrice, OvernightPrice, Amenities})
 export const GetCottage = () => api.get('/cottage/get')
+export const GetCottageById = (id) => api.get(`/cottage/get/${id}`)
 export const DeleteCottage = (id) => api.delete(`/cottage/delete/${id}`)
+export const UpdateCottage = (id) => api.put(`/cottage/update/${id}`)
