241084dc1014c97f3ab4730fc4f92cf59c96223f
Code-W-Gab <gabconcepcion02@gmail.com>
2026-04-26 23:12:17 +0800

    Add Info about Explore

--- frontend/src/Components/User/Explore.jsx
+++ frontend/src/Components/User/Explore.jsx
@@ -1,7 +1,11 @@
 export default function Explore() {
   return(
-    <main>
-      Explore
+    <main className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold">Your Perfect Escape Awaits</h1>
+        <p className="text-2xl my-8">Discover tranquility in our luxury cottages and cabins nestled in paradise</p>
+        <button className="bg-gray-200 px-10 py-4 rounded-4xl text-xl font-semibold text-blue-500">Explore Accommodations</button>
+      </div>
     </main>
   )
 }
