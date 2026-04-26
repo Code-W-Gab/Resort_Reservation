f9c0625dcb4ab8afc06ff8f787ac904c250fb63b
Code-W-Gab <gabconcepcion02@gmail.com>
2026-04-26 23:04:43 +0800

    Add explore file for layout

--- frontend/src/Pages/User/HomePage.jsx
+++ frontend/src/Pages/User/HomePage.jsx
@@ -1,9 +1,11 @@
+import Explore from "../../Components/User/Explore";
 import Header from "../../Layout/Header";
 
 export default function HomePage() {
   return(
     <main>
       <Header/>
+      <Explore/>
     </main>
   )
 }
