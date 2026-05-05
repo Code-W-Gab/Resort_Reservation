275db5ad94be15cfe313d4a18fbfc1c2ba16351b
Code-W-Gab <gabconcepcion02@gmail.com>
2026-05-05 23:05:54 +0800

    fetch cottage

--- frontend/src/Pages/User/HomePage.jsx
+++ frontend/src/Pages/User/HomePage.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import AccommodationList from "../../Components/User/AccommodationList";
 import Accommodations from "../../Components/User/Accommodations";
 import Explore from "../../Components/User/Explore";
 import Info from "../../Components/User/Info";
 import Header from "../../Layout/Header";
+import { GetCottage } from "../../Service/cottageService";
 
 export default function HomePage() {
+  const [cottages, setCottages] = useState([])
+
+  const fetchCottage = () => {
+    GetCottage()
+      .then((res) => {
+        setCottages(res.data)
+      })
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    fetchCottage()
+  }, [])
+
+
   return(
     <main>
       <Header/>
